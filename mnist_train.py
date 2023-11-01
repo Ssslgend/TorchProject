abdfa1902cc747c13a3fc5fd267784b4fe4c57b2
ssslgend <411001514@qq.com>
2023-11-01 22:04:30 +0800

    init

---
+++ mnist_train.py
@@ -0,0 +1,15 @@
+import torch
+from torch import nn
+from  torch.nn import  functional as F
+from  torch import  optim
+
+import torchvision
+from matplotlib import pyplot as plt
+from  utils import plot_curve,one_hot,plot_image
+batch_size = 512
+train_loader = torch.utils.data.DataLoader(torchvision.datasets.MNIST('mnist_data',train=True,download=True,transform=torchvision.transforms.Compose(
+    [ torchvision.transforms.ToTensor(),torchvision.transforms.Normalize((0.1307,),(0.3801,))]
+    )),batch_size=batch_size,shuffle=True)
+test_loader = torch.utils.data.DataLoader(torchvision.datasets.MNIST('mnist_data',train=True,download=True,transform=torchvision.transforms.Compose(
+    [ torchvision.transforms.ToTensor(),torchvision.transforms.Normalize((0.1307,),(0.3801,))]
+    )),batch_size=batch_size,shuffle=True)
